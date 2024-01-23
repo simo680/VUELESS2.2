e61632359f6c4cbd08484678cc92a42208ede111
Simo680 <erik.simonyan2005@gmail.com>
2024-01-23 17:02:36 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Counter></Counter>
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+
+export default {
+  compontents: {
+    Counter
+  },
+  components: { Counter }
+}
+</script>
+
+<style scoped></style>
